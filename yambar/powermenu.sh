3516bf6b1182cf39d85391b289cffb6056ef91e7
addy-dclxvi <addy4rock@gmail.com>
2022-06-16 19:22:48 +0100

    sfwbar and yambar

--- yambar/powermenu.sh
+++ yambar/powermenu.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
-op=$( echo -e " Lock" | wofi -i --dmenu | awk '{print tolower($2)}' )
+op=$( echo -e " Lock\n Screenshot" | wofi -i --location=5 -H 70 -W 35 -x -80 -y -40 --dmenu | awk '{print tolower($2)}' )
 
 case $op in 
         lock)
-				swaylock -i /home/marco/bg2.jpg
+				swaylock -i /home/marco/bg.jpg
+                ;;
+        screenshot)
+                grim -g "$(slurp)" - | swappy -f -
                 ;;
 esac
